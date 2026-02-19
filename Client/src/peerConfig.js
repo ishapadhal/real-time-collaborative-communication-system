d572b50116081800168a2a3e38484bac80b842f7
Isha Padhal <ishapadhal2004@gmail.com>
2026-02-20 00:39:38 +0530

    added TURN server for WebRTC connectivity

--- Client/src/peerConfig.js
+++ Client/src/peerConfig.js
@@ -18,13 +18,26 @@ const createPeerConnection = () => {
         debug: 3,
         config: {
             iceServers: [
-                { urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'stun:stun1.l.google.com:19302' },
-                { urls: 'stun:stun2.l.google.com:19302' },
-                { urls: 'stun:stun3.l.google.com:19302' },
-                { urls: 'stun:stun4.l.google.com:19302' }
-            ]
-        }
+             { urls: "stun:stun.l.google.com:19302" },
+
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turns:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    }
+  ]
+}
+
     });
 
     peer.on('error', (error) => {
